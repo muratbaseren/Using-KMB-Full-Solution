e014f145487039edecd0df73cf681c7abef6d769
kadirmuratbaseren <kadirmuratbaseren@gmail.com>
2016-08-23 12:59:18 +0300

    Add project files.

---
+++ Using-KMB-Full-Solution/Filters/AuthAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MuhendisAsci.Web.Filters
+{
+    public class AuthAttribute : ActionFilterAttribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationContext filterContext)
+        {
+            if (filterContext.HttpContext.Session["sys_login"] == null)
+            {
+                //string returnUrl = HttpUtility.HtmlEncode(filterContext.HttpContext.Request.Url.ToString());
+                //filterContext.Result = new RedirectResult("/Home/Login?returnUri=" + returnUrl);
+                filterContext.Result = new RedirectResult("/Home/Login");
+            }
+        }
+    }
+}
---
+++ Using-KMB-Full-Solution/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Using_KMB_Full_Solution.MvcApplication" Language="C#" %>
